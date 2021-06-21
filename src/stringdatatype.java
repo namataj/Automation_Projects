2fe3704269c54330a6111f045a776a88a25b6d72
James Namata <namatjames@gmail.com>
2021-06-21 15:34:48 -0400

    first commit

---
+++ src/stringdatatype.java
@@ -0,0 +1,25 @@
+
+public class stringdatatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String x= "I am the first child to my parents.";
+		System.out.println(x.toLowerCase());
+		
+		
+		int a=5;
+		int b=10;
+		int z=a + b;
+		System.out.println(z);
+		if (a>b) {
+			System.out.println("Pass");
+			
+		} else {
+			System.out.println("Fail");
+
+		
+			
+			}
+		}
+}
